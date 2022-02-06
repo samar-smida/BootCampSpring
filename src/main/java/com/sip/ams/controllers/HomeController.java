11bedd286218eb69314c14de559be8cbe9fff146
Samar Smida <samarsmida3@gmail.com>
2022-02-06 14:42:07 +0100

    the first project spring MVC

---
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -0,0 +1,24 @@
+
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HomeController {
+	
+	@RequestMapping("/info") //ce que je tape dans url
+	public String info( )
+	{
+		return "home/info";
+	}
+	
+	@RequestMapping("/info2")
+	public String affichage( )
+	{
+		return "home/affichage";
+	}
+	
+
+}
