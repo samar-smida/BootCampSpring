8792f7d987e81726197c67aebc78744f5a6abcae
Samar Smida <samarsmida3@gmail.com>
2022-02-06 15:55:47 +0100

    the second change

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,7 +1,10 @@
 
 package com.sip.ams.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@RequestMapping("/info") //ce que je tape dans url
-	public String info( )
+	public String info(Model model )
 	{
+		String formation="Fullstack";
 		System.out.println("affiche info");
+		model.addAttribute("workshop",formation);
 		return "home/info";
 	}
 	
@@ -20,6 +25,22 @@ public class HomeController {
 	{
 		return "home/affichage";
 	}
+	
+	@RequestMapping("/info3") //ce que je tape dans url
+	public String infotable(Model model )
+	{
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("oca");
+		list.add("ocp");
+		list.add("spring");
+		list.add("angular");
+		
+		System.out.println("affiche les info de tableau");
+		model.addAttribute("list",list);
+		return "home/info";
+	}
+	
+	
 	
 
 }
